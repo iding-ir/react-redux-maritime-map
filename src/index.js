ad2a376a0e88631a0c759b1813698147d9c68a94
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-05-10 21:31:45 +0430

    Add Mapbox hacks

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { Provider } from "react-redux";
 import "stylecraft/dist/stylecraft.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 
-import "./index.css";
-import "./hack.css";
+import "./css/index.css";
+import "./css/mapbox-hacks.css";
+import "./css/hacks.css";
 import App from "./components/app";
 import store from "./stores";
 
